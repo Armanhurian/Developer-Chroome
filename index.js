d30f5865471d337a47327fbb5fbe48bc64c3e247
Arman <hurianarmann@gmail.com>
2022-11-06 01:17:17 +0330

    add js

---
+++ index.js
@@ -0,0 +1,28 @@
+let reactElement =document.getElementById('reactt');
+let jsElement =document.getElementById('jss')
+let htmlElement =document.getElementById('htmll');
+let solidityElement =document.getElementById('solidityy');
+
+
+
+reactElement.style.backgroundColor ='rgb(33, 123, 168)'
+reactElement.style.borderRadius ='5px'
+reactElement.style.width ='50px'
+
+
+
+
+jsElement.style.backgroundColor='yellow'
+jsElement.style.borderRadius ='5px'
+jsElement.style.width ='50px'
+
+
+htmlElement.style.backgroundColor='orange'
+htmlElement.style.borderRadius ='5px'
+htmlElement.style.width ='80px'
+
+
+solidityElement.style.backgroundColor='rgba(64, 151, 24, 0.728)'
+solidityElement.style.borderRadius ='5px'
+solidityElement.style.width ='80px'
+
