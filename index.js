ef233eb56933cd36e382957f683e01556fda6d91
Arman <hurianarmann@gmail.com>
2022-11-27 19:52:26 +0330

    Add New Commit in Developer Chroome

--- index.js
+++ index.js
@@ -51,3 +51,62 @@ let solidityEventListener= solidityElement.addEventListener('click',function(){
     solidityElement.style.width ='80px'
 })
 
+let myLogin = document.querySelector('.myLogin')
+let myManu = document.querySelector('.myManu')
+
+
+myLogin.addEventListener('click',function(event){
+    event.target.style.display='none'
+    myManu.style.display='block'
+})
+
+let loginFormBtnSubmit = document.querySelector('.loginFormBtnSubmit')
+let loginFormBtnCancel = document.querySelector('.loginFormBtnCancel')
+let myNameInput = document.querySelector('.myNameInput')
+let myAgeInput = document.querySelector('.myAgeInput')
+let loginFormPage = document.querySelector('.loginFormPage')
+let registry  = document.querySelector('.registry')
+
+let Profile  = document.querySelector('.Profile')
+let myProfileBox = document.querySelector('.myProfileBox')
+let myProfileCancelBox = document.querySelector('.myProfileCancelBox')
+let myNameProfileBox = document.querySelector('.myNameProfileBox')
+let myAgeProfileBox = document.querySelector('.myAgeProfileBox')
+let emptyError =document.querySelector('.emptyError')
+
+
+
+registry.addEventListener('click',function(){
+    loginFormPage.style.display='block'
+    myProfileBox.style.display='none' 
+})
+
+loginFormBtnCancel.addEventListener('click',function(){
+    loginFormPage.style.display='none'
+})
+
+loginFormBtnSubmit.addEventListener('click',function(event){
+    
+    
+    if(myNameInput.value ==''|| myAgeInput.value ==''){
+        emptyError.innerHTML='Please log in to your user panel first'
+        emptyError.style.color='red'
+    }else{
+        myNameProfileBox.innerHTML ='name is : ' + myNameInput.value
+        myAgeProfileBox.innerHTML ='age is : ' + myAgeInput.value
+        emptyError.style.display='none'
+    }
+    
+})
+
+
+
+
+Profile.addEventListener('click',function(){
+    myProfileBox.style.display='block'
+    loginFormPage.style.display='none'  
+})
+
+myProfileCancelBox.addEventListener('click',function(){
+    myProfileBox.style.display='none'
+})
